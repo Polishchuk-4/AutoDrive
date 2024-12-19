cef4415c47349edeaf06ed186e6f901355ac0827
Denys Polishchuk <polischyk0000@gmail.com>
2024-12-19 15:06:10 +0200

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: "autodrivetest-44677.firebaseapp.com",
+  projectId: "autodrivetest-44677",
+  storageBucket: "autodrivetest-44677.firebasestorage.app",
+  messagingSenderId: "195564648759",
+  appId: "1:195564648759:web:d6ae5fa19ed718cce2c2b2",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
