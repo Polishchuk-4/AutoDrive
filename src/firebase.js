5b9c3809bd7ceb9a44459a0a151d9d945b8dd93d
Denys Polishchuk <polischyk0000@gmail.com>
2024-12-25 14:06:14 +0200

    change import config firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,17 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const API_KEY = import.meta.env.VITE_API_KEY;
-
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: "autodrivetest-44677.firebaseapp.com",
-  projectId: "autodrivetest-44677",
-  storageBucket: "autodrivetest-44677.firebasestorage.app",
-  messagingSenderId: "195564648759",
-  appId: "1:195564648759:web:d6ae5fa19ed718cce2c2b2",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
-
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
